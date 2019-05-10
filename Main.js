7bc3e103fe34e72d25d376b2ab687aa8b027b53e
John Thomas Valanidas <john.valanidas@gmail.com>
2019-05-10 13:17:00 -0400

    - cleaned up some code and moved some small stuff around.

--- Main.js
+++ Main.js
@@ -1,34 +1,25 @@
+// Discord set up
 const Discord = require("discord.js");
 const client  = new Discord.Client();
+
+// Aux Library for files
 const fs = require("fs");
 
-// The secret token sould be posted into the .key file in Otter bot's main folder.
-fs.readFile(".key", "utf8", function(error, data) {
-  client.login(data);
-});
 
-
-// Ready function
-client.on("ready", () => {
-    console.log("The Otters have synced up.");
-    console.log("They are ready for commands!");
-    updateGuilds();
-});
-
+// Bot object that will house commands
 let Bot = require('./Bot.js');
 
-// CREATE A GOD DAMN CONFIG FILE FOR THIS
 // bot commands
-let Ping          = require('./Commands/Ping.js');
-let OtterFacts    = require('./Commands/OtterFacts.js');
-let TopReddit     = require('./Commands/TopReddit.js');
-let Roll          = require('./Commands/Roll.js');
-let Information   = require('./Commands/Information.js');
-let Help          = require('./Commands/Help.js');
-let Commands      = require('./Commands/Commands.js');
-let SteamTogether = require('./Commands/SteamTogether.js');
-let ClearChannel  = require('./Commands/ClearChannel.js');
-let SoundBoard    = require('./Commands/Soundboard.js');
+const Ping          = require('./Commands/Ping.js');
+const OtterFacts    = require('./Commands/OtterFacts.js');
+const TopReddit     = require('./Commands/TopReddit.js');
+const Roll          = require('./Commands/Roll.js');
+const Information   = require('./Commands/Information.js');
+const Help          = require('./Commands/Help.js');
+const Commands      = require('./Commands/Commands.js');
+const SteamTogether = require('./Commands/SteamTogether.js');
+const ClearChannel  = require('./Commands/ClearChannel.js');
+const SoundBoard    = require('./Commands/Soundboard.js');
 
 //aux bot functions
 let ChatLogging   = require('./ChatLogging.js');
@@ -45,31 +36,26 @@ let botCommands = {
     'SoundBoard': SoundBoard
 };
 
-let commands = [];
-// TODO: Make way of creating command objects and then add the objects to the array.
-let test  = new Ping("Ping", "Checks if the bot is working at all");
-let test1 = new OtterFacts("OtterFacts", "Gives a random otter fact with an optional description");
-let test2 = new TopReddit("TopReddit", "Returns the top posts from either the front page or a subreddit");
-let test3 = new Roll("Roll", "Rolls a die of a specified size");
-let test4 = new Help("Help", "Gives information on each command.");
-let test5 = new Information("Information", "Information about the bot");
-let test6 = new Commands("Commands", "Gives a list of commands");
-let test7 = new SteamTogether("SteamTogether","Checks and prints what games two users have");
-let test8 = new ClearChannel("ClearChannel","Give you control over the messages in any channel");
-let test9 = new SoundBoard("SoundBoard", "TODO: add soundboard desc");
-channelCommands = [test, test1, test2, test3, test4, test5, test6, test7, test8, test9];
+// The secret token sould be posted into the .key file in Otter bot's main folder.
+fs.readFile(".key", "utf8", function(error, data) {
+    client.login(data);
+});
 
-// Help and Commands need to reference to all of the commands so they can
-// loop through them.
-test4.linkCommands(channelCommands);
-test6.linkCommands(channelCommands);
+
+// Ready function
+client.on("ready", () => {
+    console.log("The Otters have synced up.");
+    console.log("They are ready for commands!");
+    updateGuilds();
+});
+
 
 let connectedGuilds = {};
 //Auxillary bot functions
 let logger = new ChatLogging();
 
 // Main loop
-// Interaction with messages from clint
+// Interaction with messages from client
 client.on("message", function (message){
   // Messages need to be logged first in order to check if a command crashes
   // the bot
@@ -108,19 +94,34 @@ function updateGuilds() {
 }
 
 /**
- * Updates a specific guild with it's corrisonding config. If it is not found it loads the default one.
+ * Updates a specific guild with it's corresponding config. If it is not found it loads the default one.
  * @param guild
  */
 function updateGuildBot(guild) {
     let mBot =  new Bot(guild);
+    // data contains the raw data from either a database or a default value
     let data = getGuildConfig(guild.id);
     let json = JSON.parse(data);
+    // grab top level property names that indicate a command name.
+    // Their associated properties are a  js object that will be parsed
     let commands = Object.getOwnPropertyNames(json)
     for (let command of commands.values()) {
         // must create a ref from the botCommands object since it holds the import.
         // from there an actual object can be instantiated.
-        let ref  = botCommands[command];
-        mBot.addCommand(new ref(json[command].name, json[command].description))
+
+        //debug statement for showing the linking commands.
+        console.log(guild.name + ": Linking command - " + command);
+        try {
+            let ref  = botCommands[command];
+            refCommand = new ref(json[command].name, json[command].description);
+            mBot.addCommand(refCommand);
+            if (json[command].link) {
+                refCommand.linkCommands(mBot.commands);
+            }
+        }
+        catch(error) {
+            console.error(error);
+        }
     }
     connectedGuilds[guild.id] = mBot;
 }
